29bbbc5934e0715172f9475ae01293b1864e5656
malucien <124107514+malucien@users.noreply.github.com>
2023-03-21 18:53:57 -0500

    First commit

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -2,12 +2,13 @@ import react from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle} from "reactstrap";
 
 
-const campsiteCard = (props) => {
+const campsiteCard = ({campsite}) => {
+  const {image, name } = campsite;
   return(
     <Card>
-      <CardImg with = "100%" src={props.campsite.image} alt={props.campsite.name}/>
+      <CardImg with = "100%" src={image} alt={name}/>
       <CardImgOverlay>
-        <CardTitle>{props.campsite.name}</CardTitle>
+        <CardTitle>{name}</CardTitle>
       </CardImgOverlay>
     </Card>
   );
