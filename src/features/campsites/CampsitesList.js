732beb254924164df16f7ddda37bbdc286f7638f
malucien <124107514+malucien@users.noreply.github.com>
2023-03-22 20:46:31 -0500

    First commit

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Row, Col } from 'reactstrap';
 import CampsiteCard from './CampsiteCard';
-import { selectAllCampsites } from './campsitesSlice',
+import { selectAllCampsites } from './campsitesSlice';
 
 const CampsitesList = () => {
   const campsites = selectAllCampsites();
--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -3,3 +3,7 @@ import {CAMPSITES } from '../../app/shared/CAMPSITES';
 export const selectAllCampsites = () => {
   return CAMPSITES;
 };
+
+export const selectRandomCampsite = () => {
+  return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+}
