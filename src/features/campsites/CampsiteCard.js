944462da2172d453f21b3f832085092efb4e9060
malucien <124107514+malucien@users.noreply.github.com>
2023-03-26 14:41:27 -0500

    First commit

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,16 +1,19 @@
 import react from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle} from "reactstrap";
+import {Link} from 'react-router-dom';
 
 
 const campsiteCard = ({campsite}) => {
-  const {image, name } = campsite;
+  const {id, image, name } = campsite;
   return(
-    <Card>
-      <CardImg with = "100%" src={image} alt={name}/>
-      <CardImgOverlay>
-        <CardTitle>{name}</CardTitle>
-      </CardImgOverlay>
-    </Card>
+    <Link to={`${id}`}>
+      <Card>
+        <CardImg with = "100%" src={image} alt={name}/>
+        <CardImgOverlay>
+          <CardTitle>{name}</CardTitle>
+        </CardImgOverlay>
+      </Card>
+    </Link>
   );
 };
 export default campsiteCard;
