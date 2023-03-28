4244419f11ce07e1e5d9b712c6b7c8b0ed5a6237
malucien <124107514+malucien@users.noreply.github.com>
2023-03-27 20:43:15 -0500

    First commit

--- src/features/partners/PartnersSlice.js
+++ src/features/partners/PartnersSlice.js
@@ -4,9 +4,6 @@ export const selectAllPartners = () =>{
    return PARTNERS;
 };
 
-export const selectPartnerById = (id) => {
-  return PARTNERS.find((partner) => partner.id === parseInt(id));
-}
 
 export const selectFeaturedPartner = () => {
   return PARTNERS.find((partner) => partner.featured);
