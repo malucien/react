7d987c4041eafd816facafa31ea9c3f458efd226
malucien <124107514+malucien@users.noreply.github.com>
2023-04-01 13:04:05 -0500

    First commit

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,30 @@
+import {Container, Col, Row} from 'reactstrap';
+import SubHeader from "../components/SubHeader";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>
+  return (
+    <Container>
+      <SubHeader current="Contact Us"/>
+      <Row className='row-content align-items-center'>
+        <Col>
+          <h5>Our Address</h5>
+          <address>
+            1 Nucamp Way
+            <br />
+            Seattle, WA 98001
+            <br />
+            U.S.A.
+          </address>
+        </Col>
+        <Col>
+        
+        </Col>
+      </Row>
+      <Row className='row-content'>
+
+      </Row>
+    </Container>
+  )
 };
 
 export default ContactPage;
