be7039c11bfb4394c981da8473c7c439cac3e7e1
malucien <124107514+malucien@users.noreply.github.com>
2023-04-01 21:16:39 -0500

    First commit

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -16,55 +16,79 @@ const ContactForm = () => {
     >
       <Form>
         <FormGroup row>
-          <Label>
-
+          <Label htmlFor="firstName" md="2">
+            First Name
           </Label>
           <Col md="10">
-          
+            <Field
+              name="firstName"
+              ClassName="form-control" 
+            />
+
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label>
-
+          <Label htmlFor="lastName" md="2">
+            Last Name
           </Label>
           <Col md="10">
-          
+            <Field
+              name="lastName"
+              ClassName="form-control" 
+            />
+
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label>
-
+          <Label htmlFor="phoneNum" md="2">
+            Phone
           </Label>
           <Col md="10">
-          
+            <Field
+              name="phoneNum"
+              ClassName="form-control" 
+            />
+
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label>
-
+          <Label htmlFor="email" md="2">
+            Email
           </Label>
           <Col md="10">
-          
+            <Field
+              name="email"
+              ClassName="form-control" 
+            />
+
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label>
-
+          <Label check md={{size:4, offset:2}}>
+            May we contact you?
           </Label>
           <Col md="4">
-          
+            <Field
+              ClassName="form-control" 
+            />
+
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label>
-
+          <Label htmlFor="feedBack" md="2">
+            Your Feedback
           </Label>
           <Col md="10">
-          
+            <Field
+              ClassName="form-control" 
+            />
+
           </Col>
         </FormGroup>
         <FormGroup row>
-          
+          <Field
+              ClassName="form-control" 
+            />
         </FormGroup>
         
       </Form>
