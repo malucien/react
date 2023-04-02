7ea56b7bf277e248ff8de4358a818a9044931118
malucien <124107514+malucien@users.noreply.github.com>
2023-04-01 23:07:02 -0500

    commentForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,6 @@
 import { Button, Label, Col, FormGroup } from 'reactstrap';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
-import { validateContactForm } from '../utils/ValidateContactForm';
+import { validateContactForm } from '../utils/validateContactForm';
 
 const ContactForm = () => {
   const handleSubmit = (values, {resetForm}) => {
