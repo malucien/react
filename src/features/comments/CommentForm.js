3c651fce828a75cf3577fbaf104e420f90fc90d5
malucien <124107514+malucien@users.noreply.github.com>
2023-04-01 23:18:59 -0500

    First commit

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
+import { Formik, Field, Form } from 'formik';
 
 const CommentForm = ({ campsiteId }) => {
   const [modalOpen, setModalOpen] = useState(false);
 
-  return (
-    <>
-      <Button outline onClick={() => setModalOpen(true)}>
-        <i className='fa fa-pencil fa-lg' /> Add Comment
-      </Button>
-      <Modal isOpen={modalOpen}>
-        <ModalHeader toggle={() => setModalOpen(false)}>Add Comment</ModalHeader>
-        <ModalBody>campsite: {campsiteId}</ModalBody>
-      </Modal>
-    </>
-  );
-};
+  const handleSubmit = values => {
+    const comment = {
+      campsiteId: parseInt(campsiteId),
+      rating: values.rating,
+      author: values.author,
+      text: values.commentText
+    };
+    console.log(comment);
+    setModalOpen(false);
+  };
 
-export default CommentForm;
+ 
+
