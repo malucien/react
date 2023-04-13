7b7d322b5baab5e235ff8599e1aa5012d62c4b81
malucien <124107514+malucien@users.noreply.github.com>
2023-04-12 23:50:54 -0500

    First commit

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -48,10 +48,7 @@ const CommentForm = ({ campsiteId }) => {
                   <option>3</option>
                   <option>4</option>
                   <option>5</option>
-                  <option>5</option>
-                  <option>5</option>
-                  <option>5</option>
-                  <option>5</option>
+                 
                   
                 </Field>
                 <ErrorMessage name="rating">{(msg) => <p className="text-danger">{msg}</p>}</ErrorMessage>
