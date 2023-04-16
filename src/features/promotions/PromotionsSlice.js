8078baf0b87d681997e9ef91906d8a0911c8724e
malucien <124107514+malucien@users.noreply.github.com>
2023-04-16 17:15:42 -0500

    First commit

--- src/features/promotions/PromotionsSlice.js
+++ src/features/promotions/PromotionsSlice.js
@@ -1,13 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit';
-import {PROMOTIONS} from '../../app/shared/PROMOTIONS';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { baseUrl } from '../../app/shared/baseUrl';
+import { mapImageURL } from '../../utils/mapImageURL';
+
+export const fetchPromotions = createAsyncThunk(
+  'promotions/fetchPromotions',
+  async () => {
+    const response = await fetch(baseUrl + 'promotions');
+    if (!response.ok) {
+      return Promotion.reject('Unable to fetch, status:' + response.status);
+    }
+    const data = await response.json();
+    return data;
+  }
+  
+);
 
 const initialState = {
-  promotionsArray: PROMOTIONS
+  promotionsArray:[],
+  isLoading: true,
+  errMsg: ''
+
 };
 
 const promotionsSlice = createSlice({
   name: 'promotions',
-  initialState
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchPromotions.pending]: (state) => {
+      state.isLoading = true;
+    }
+  }
 });
 
 export const promotionsReducer = promotionsSlice.reducer;
