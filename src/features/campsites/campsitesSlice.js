4ad35e766b0220c0a46e907ca414679bf7ed74c7
malucien <124107514+malucien@users.noreply.github.com>
2023-04-16 12:24:31 -0500

    First commit

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -6,17 +6,39 @@ export const fetchCampsites = createAsyncThunk(
   'campsites/fetchCampsite',
   async () => {
     const response = await fetch(baseUrl + 'campsite');
+    if (!response.ok) {
+      return Promise.reject('Unable to fetch, status:' + response.status);
+    }
+    const data = await response.json();
+    return data;
   }
-)
+);
 
 
 const initialState = {
-  campsitesArray: CAMPSITES
+  campsitesArray: [],
+  isLoading: true,
+  errMsg:''
 };
 
 const campsitesSlice = createSlice({
   name:'campsites',
-  initialState
+  initialState,
+  reducers:{},
+  extraReducers: {
+    [fetchCampsites.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchCampsites.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      satisfies.errMsg = '';
+      state.campsitesArray = mapImageURL(action.payload);
+    },
+    [fetchCampsites.rejected]: (state, action) => {
+      state.isLoading = false;
+
+state.errMsg = action.error ? action.error.message : 'Fetch failed'    }
+  }
 });
 
 export const campsitesReducer = campsitesSlice.reducer;
